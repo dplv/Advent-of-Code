618430ef222cf0a5b286af3fa6fdfd7efbb97de3
dpopoks <75681633+dpopoks@users.noreply.github.com>
2021-12-01 16:00:04 +0200

    Update day1.py
    Day 1 solution

--- 2021/day 1/day1.py
+++ 2021/day 1/day1.py
@@ -4,4 +4,9 @@ dir_path = os.path.realpath(__file__)
 path, filename = os.path.split(dir_path)                
 data = open(path + '\input.txt').read().split('\n')
 
-print(data)
+result_p1 = [int(i) - int(j) for i, j in zip(data[1:], data[:-1]) if int(i) - int(j) > 0]
+print(len(result_p1))
+
+sums = [int(i) + int(j) + int(k) for i, j, k in zip(data, data[1:], data[2:])]
+result_p2 = [int(i) - int(j) for i, j in zip(sums[1:], sums[:-1]) if int(i) - int(j) > 0]
+print(len(result_p2))
