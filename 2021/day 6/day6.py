558bcc04fac3272afe409032234a641359acba96
dpopoks <75681633+dpopoks@users.noreply.github.com>
2021-12-25 18:12:52 +0200

    Day 6 update

---
+++ 2021/day 6/day6.py
@@ -0,0 +1,18 @@
+import os
+
+dir_path = os.path.realpath(__file__)
+path, filename = os.path.split(dir_path)
+input = open(path + '\\input.txt')
+
+fishes = [0 for x in range(9)]
+
+for f in next(input).split(','):
+    fishes[int(f)] += 1
+days = 256
+
+for d in range(days):
+    new = fishes.pop(0)
+    fishes[6] += new
+    fishes.append(new)
+
+print(sum(fishes))
