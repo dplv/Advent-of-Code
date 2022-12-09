da17d16347202759ba691116032374dbee9c2676
dpopoks <75681633+dpopoks@users.noreply.github.com>
2022-12-09 23:27:52 +0200

    Day 7

--- 2022/day 7/day7.py
+++ 2022/day 7/day7.py
@@ -1,44 +1,52 @@
 import os
 from collections import defaultdict
 
+
+def directory_size(dir: str) -> int:
+    size = 0
+    for element in tree[dir]:
+        if element.startswith('dir'):
+            element = element.replace('dir ', '')
+            element = '/' + element if dir == '/' else dir + '/' + element
+            if element in sizes:
+                size += sizes[element]
+            else:
+                size += directory_size(element)
+        else:
+            size += int(element.split(' ')[0])
+    return size
+
+
 dir = os.path.dirname(__file__)
 lines = open(dir + '\input.txt', 'r').readlines()
 lines = [line.replace('\n', '') for line in lines]
 
 tree = defaultdict(list)
 sizes = defaultdict(int)
-print(lines)
+# print(lines)
 
-curr_dir = '/'
+path = ''
 
 for line in lines:
     if line.startswith('$ cd ..'):
-        new_dir = [i for i in tree if curr_dir in tree[i]]
-        curr_dir = new_dir
+        path = path.rsplit("/", 1)[0]
     elif line.startswith('$ cd'):
-        dir = line.replace('$ cd ', '')
-        if dir.isalpha() or dir == '/':
-            tree[dir] = []
-            curr_dir = dir   
+        folder = line.replace('$ cd ', '')
+        folder = '' if folder == '/' else folder
+        path += '/' if path != '/' else ''
+        path += folder
     elif not line.startswith('$'):
-        tree[curr_dir].append(line)
-
-def return_dir_size(dir: str) -> int:
-    size = 0
-    for element in tree[dir]:
-        if element.startswith('dir'):
-            element = element.replace('dir ', '')
-            if element in sizes:
-                size += sizes[element]
-            else:
-                size += return_dir_size(element)
-        else:
-            size += int(element.split(' ')[0])
-    return size
-
-print(tree)
+        tree[path].append(line)
 
 for d in tree:
-    sizes[d] = return_dir_size(d)
+    sizes[d] = directory_size(d)
 
-print(sum([val for val in sizes.values() if val <= 100000]))
+print(sum([size for size in sizes.values() if size <= 100000])) #part 1
+
+MAX_SIZE = 70000000
+REQUIRED = 30000000
+USED     = sizes['/']
+UNUSED   = MAX_SIZE - USED
+MISSING  = REQUIRED - UNUSED
+
+print(min([size for size in sizes.values() if size >= MISSING])) #part 2
