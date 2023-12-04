ae4e5f2a3b9c3b18b7d1a576c84c4c87c2a34a7f
dpopoks <75681633+dplv@users.noreply.github.com>
2023-12-04 22:52:39 +0200

    2023 Day 4

---
+++ 2023/day 4/day4.py
@@ -0,0 +1,35 @@
+import os
+import re
+
+
+def get_file() -> list:
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    file = open(__location__ + os.sep + 'input.txt')
+    return file.readlines()
+
+
+def solution(data: list) -> (int, int):
+    sum_p1 = 0
+    pattern = r'[:](.*)[|](.*)'
+
+    COPIES = [0] * len(data)
+
+    for id, line in enumerate(data):
+        numbers = re.findall(pattern, line)
+
+        card = [int(x) for x in numbers[0][0].split(' ') if x.isdigit()]
+        wins = [int(x) for x in numbers[0][1].split(' ') if x.isdigit()]
+        points = len([1 for x in card if x in wins])
+        sum_p1 += 2 ** (points - 1) if points > 0 else 0
+
+        COPIES[id] += 1
+        for c in range(id + 1, id + points + 1):
+            COPIES[c] += COPIES[id]
+
+    return sum_p1, sum(COPIES)
+
+
+if __name__ == '__main__':
+    input = get_file()
+
+    print(solution(input))
