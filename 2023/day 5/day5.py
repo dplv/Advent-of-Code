623bcd109551af5fb8051dc138e208e758bafd4c
dpopoks <75681633+dplv@users.noreply.github.com>
2023-12-06 16:34:03 +0200

    2023 Day 5 Part 2

--- 2023/day 5/day5.py
+++ 2023/day 5/day5.py
@@ -13,6 +13,7 @@ def get_file() -> list:
 def solution(data: list) -> (int, int):
     seeds = [int(x) for x in re.findall(r'\d+', data[0])]
     seeds_ranges = [(seeds[i], seeds[i] + seeds[i + 1]) for i in range(0, len(seeds), 2)]
+    total_seeds = sum(seeds[1::2])
     maps = [m.split('\n')[1:] for m in data[1:]]
 
     for m in maps:
@@ -23,7 +24,21 @@ def solution(data: list) -> (int, int):
                 if seed in range(map_line[1], map_line[1] + map_line[2]):
                     seeds[s] = seed + (map_line[0] - map_line[1])
 
-    return min(seeds)
+    min_location = float('inf')
+    c = 0
+    for start, end in seeds_ranges:
+        for seed in range(start, end):
+            c += 1
+            print(f'Scanning: {c:>10}/{total_seeds} ({min_location})')
+            for m in maps:
+                for map_line in m:
+                    map_line = [int(x) for x in map_line.split()]
+                    if seed in range(map_line[1], map_line[1] + map_line[2]):
+                        seed = seed + (map_line[0] - map_line[1])
+                        break
+            min_location = min(seed, min_location)
+            
+    return min(seeds), min_location
 
 
 if __name__ == '__main__':
