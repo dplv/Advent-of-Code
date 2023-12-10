f848f52c01709dbf4132c6557f9e0e12410342c3
dpopoks <75681633+dplv@users.noreply.github.com>
2023-12-10 22:22:16 +0200

    2023 Day 9

--- 2023/day 9/day9.py
+++ 2023/day 9/day9.py
@@ -1,5 +1,4 @@
 import os
-import math
 
 
 def get_file() -> list:
@@ -13,11 +12,44 @@ def get_file() -> list:
 
 
 def part1(data: list) -> int:
+    value = 0
     for history in data:
-        diff = [history[i] - history[i-1] for i in range(1, len(history))]
-        print(diff)
+        
+        levels = [history, ]
+        while not all([i == 0 for i in history]):
+            diff = [history[i] - history[i-1] for i in range(1, len(history))]
+            levels.append(diff)
+            history = diff
+        
+        levels = levels[::-1]
+        for i, _ in enumerate(levels[1:]):
+            levels[i + 1].append(levels[i+1][-1] + levels[i][-1])
+        value += levels[-1][-1]
+    
+    return value
+
+
+def part2(data: list) -> int:
+    value = 0
+    for history in data:
+        history = [0] + history
+
+        levels = [history, ]
+        while not all([i == 0 for i in history[1:]]):
+            diff = [history[i] - history[i-1] for i in range(1, len(history))]
+            levels.append(diff)
+            history = diff
+
+        levels[-1][0] = 0
+        levels = levels[::-1]
+        for i, _ in enumerate(levels[1:]):
+            levels[i + 1][0] = levels[i + 1][1] - levels[i][0]
+        value += levels[-1][0]
+    
+    return value
 
 
 if __name__ == '__main__':
     input = get_file()
     print(part1(input))
+    print(part2(input))
